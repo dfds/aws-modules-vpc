990dd9accbda9341fba989a5adad0cfdb9c3f994
ranixa <ranix@dfds.com>
2023-01-17 23:30:01 +0000

    Part modulized but will probably need an egress

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,27 @@
 output "vpc_id" {
     value = aws_vpc.this.id
 }
+
+output "public_subnet_ids" {
+    value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id
+}
+
+output "data_subnet_ids" {
+    value = aws_subnet.nated[*].id
+}
+
+output "public_subnet_ranges" {
+    value = var.cidr_block_public_subnet
+}
+
+output "private_subnet_ranges" {
+    value = var.cidr_block_private_subnet
+}
+
+output "data_subnet_ranges" {
+    value = var.cidr_block_data_subnet
+}
