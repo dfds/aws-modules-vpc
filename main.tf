ed5a3df7ea7180f2464ecdebb0067346322f4ea9
Aleksandra Korczak <alekorc@dfds.com>
2023-01-18 14:07:16 +0100

    Fix route tables

--- main.tf
+++ main.tf
@@ -98,7 +98,7 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this.id
+    gateway_id = aws_nat_gateway.this[count.index].id
   }
   tags = merge(
     var.additional_tags,
