d0971065dcafbdbce1e4b6cce6c074ae3fe64785
Aleksandra Korczak <alekorc@dfds.com>
2023-01-18 11:38:51 +0100

    Refactor

--- main.tf
+++ main.tf
@@ -1,96 +1,111 @@
-terraform {
-  backend "s3" {
-    # This configuration will be filled in by Terragrunt
-  }
-}
-
 resource "aws_vpc" "this" {
-  cidr_block       = var.cidr_block
+  cidr_block           = var.cidr_block
   enable_dns_hostnames = true
-  tags = merge(var.additional_tags,{
-    Name = var.name
-  })
+  tags = merge(
+    var.additional_tags,
+    {
+      Name = var.name
+    }
+  )
 }
 
 resource "aws_subnet" "public" {
-  count = length(var.cidr_block_public_subnet)
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = "${var.cidr_block_public_subnet[count.index]}"
-  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+  count                   = length(var.cidr_block_public_subnet)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.cidr_block_public_subnet[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
-  tags = {
-    Name = "${var.name}-public-subnet-${data.aws_availability_zones.available.names[0]}"
-  }
+  tags = merge(
+    var.additional_tags,
+    {
+      Name = "${var.name}-public-subnet-${data.aws_availability_zones.available.names[0]}"
+    }
+  )
 }
 
 resource "aws_subnet" "private" {
-  count = length(var.cidr_block_private_subnet)
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = "${var.cidr_block_private_subnet[count.index]}"
-  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+  count                   = length(var.cidr_block_private_subnet)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.cidr_block_private_subnet[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
-  tags = {
-    Name = "${var.name}-private-subnet-${data.aws_availability_zones.available.names[0]}"
-  }
+  tags = merge(
+    var.additional_tags,
+    {
+      Name = "${var.name}-private-subnet-${data.aws_availability_zones.available.names[0]}"
+    }
+  )
 }
 
-resource "aws_subnet" "nated" {
-  count = length(var.cidr_block_data_subnet)
+resource "aws_subnet" "data" {
+  count             = length(var.cidr_block_data_subnet)
   vpc_id            = aws_vpc.this.id
-  cidr_block        = "${var.cidr_block_data_subnet[count.index]}"
-  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
-  tags = {
-    Name = "${var.name}-nated-subnet-${data.aws_availability_zones.available.names[count.index]}"
-  }
+  cidr_block        = var.cidr_block_data_subnet[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags = merge(
+    var.additional_tags,
+    {
+      Name = "${var.name}-data-subnet-${data.aws_availability_zones.available.names[count.index]}"
+    }
+  )
 }
 
 resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
-  tags = {
-    Name = "${var.name}-igw"
-  }
+  tags = merge(
+    var.additional_tags,
+    {
+      Name = "${var.name}-igw"
+    }
+  )
 }
-resource "aws_eip" "nat_gw" {
+resource "aws_eip" "this" {
   count = length(var.cidr_block_public_subnet)
-  vpc = true
+  vpc   = true
 }
-resource "aws_nat_gateway" "gw" {
-  count = length(var.cidr_block_public_subnet)
-  allocation_id = "${aws_eip.nat_gw[count.index].id}"
-  subnet_id     = "${aws_subnet.public[count.index].id}"
+resource "aws_nat_gateway" "this" {
+  count         = length(var.cidr_block_public_subnet)
+  allocation_id = aws_eip.this[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
 
   depends_on = [aws_internet_gateway.this]
 }
 resource "aws_route_table" "public" {
-    count = length(var.cidr_block_public_subnet)
-    vpc_id = aws_vpc.this.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.this.id
+  count  = length(var.cidr_block_public_subnet)
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+  tags = merge(
+    var.additional_tags,
+    {
+      Name = "${var.name}-public-rt"
     }
-    tags = {
-        Name = "${var.name}-public-rt"
-    }
+  )
 }
 resource "aws_route_table_association" "public" {
-    count = length(var.cidr_block_public_subnet)
-    subnet_id      = "${aws_subnet.public[count.index].id}"
-    route_table_id = "${aws_route_table.public[count.index].id}"
+  count          = length(var.cidr_block_public_subnet)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public[count.index].id
 }
 
 resource "aws_route_table" "private" {
-    count = length(var.cidr_block_private_subnet)
-    vpc_id = aws_vpc.this.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.this.id
+  count  = length(var.cidr_block_private_subnet)
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+  tags = merge(
+    var.additional_tags,
+    {
+      Name = "${var.name}-public-rt"
     }
-    tags = {
-        Name = "${var.name}-public-rt"
-    }
+  )
 }
 resource "aws_route_table_association" "private" {
-    count = length(var.cidr_block_private_subnet)
-    subnet_id      = "${aws_subnet.private[count.index].id}"
-    route_table_id = "${aws_route_table.private[count.index].id}"
+  count          = length(var.cidr_block_private_subnet)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
 }
--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,27 @@
 output "vpc_id" {
-    value = aws_vpc.this.id
+  value = aws_vpc.this.id
 }
 
 output "public_subnet_ids" {
-    value = aws_subnet.public[*].id
+  value = aws_subnet.public[*].id
 }
 
 output "private_subnet_ids" {
-    value = aws_subnet.private[*].id
+  value = aws_subnet.private[*].id
 }
 
 output "data_subnet_ids" {
-    value = aws_subnet.nated[*].id
+  value = aws_subnet.nated[*].id
 }
 
 output "public_subnet_ranges" {
-    value = var.cidr_block_public_subnet
+  value = var.cidr_block_public_subnet
 }
 
 output "private_subnet_ranges" {
-    value = var.cidr_block_private_subnet
+  value = var.cidr_block_private_subnet
 }
 
 output "data_subnet_ranges" {
-    value = var.cidr_block_data_subnet
+  value = var.cidr_block_data_subnet
 }
