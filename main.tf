6b22b6294c60daaac9ccd2920705a58b78c58b59
ranixa <ranix@dfds.com>
2023-01-18 09:54:56 +0000

    changes made to module as required

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+terraform {
+  backend "s3" {
+    # This configuration will be filled in by Terragrunt
+  }
+}
+
 resource "aws_vpc" "this" {
   cidr_block       = var.cidr_block
   enable_dns_hostnames = true
@@ -8,7 +14,7 @@ resource "aws_vpc" "this" {
 
 resource "aws_subnet" "public" {
   count = length(var.cidr_block_public_subnet)
-  vpc_id            = "${aws_vpc.this.id}"
+  vpc_id            = aws_vpc.this.id
   cidr_block        = "${var.cidr_block_public_subnet[count.index]}"
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
   map_public_ip_on_launch = true
@@ -19,7 +25,7 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "private" {
   count = length(var.cidr_block_private_subnet)
-  vpc_id            = "${aws_vpc.this.id}"
+  vpc_id            = aws_vpc.this.id
   cidr_block        = "${var.cidr_block_private_subnet[count.index]}"
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
   map_public_ip_on_launch = true
@@ -28,14 +34,35 @@ resource "aws_subnet" "private" {
   }
 }
 
+resource "aws_subnet" "nated" {
+  count = length(var.cidr_block_data_subnet)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = "${var.cidr_block_data_subnet[count.index]}"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+  tags = {
+    Name = "${var.name}-nated-subnet-${data.aws_availability_zones.available.names[count.index]}"
+  }
+}
+
 resource "aws_internet_gateway" "this" {
-  vpc_id = "${aws_vpc.this.id}"
+  vpc_id = aws_vpc.this.id
   tags = {
     Name = "${var.name}-igw"
   }
 }
+resource "aws_eip" "nat_gw" {
+  count = length(var.cidr_block_public_subnet)
+  vpc = true
+}
+resource "aws_nat_gateway" "gw" {
+  count = length(var.cidr_block_public_subnet)
+  allocation_id = "${aws_eip.nat_gw[count.index].id}"
+  subnet_id     = "${aws_subnet.public[count.index].id}"
 
+  depends_on = [aws_internet_gateway.this]
+}
 resource "aws_route_table" "public" {
+    count = length(var.cidr_block_public_subnet)
     vpc_id = aws_vpc.this.id
     route {
         cidr_block = "0.0.0.0/0"
@@ -47,70 +74,23 @@ resource "aws_route_table" "public" {
 }
 resource "aws_route_table_association" "public" {
     count = length(var.cidr_block_public_subnet)
-    subnet_id = aws_subnet.public[count.index].id
-    route_table_id = aws_route_table.public.id
+    subnet_id      = "${aws_subnet.public[count.index].id}"
+    route_table_id = "${aws_route_table.public[count.index].id}"
 }
 
-resource "aws_subnet" "nated" {
-  count = length(var.cidr_block_public_subnet)
-  vpc_id     = aws_vpc.this.id
-  cidr_block = cidrsubnet(aws_vpc.this.cidr_block[count.index], 8, 3)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  tags = {
-    Name = "${var.name}-nated-subnet-${data.aws_availability_zones.available.names[count.index]}"
-  }
-}
-
-resource "aws_eip" "nat_gw_eip_1" {
- count = length(var.cidr_block_public_subnet)
-  vpc = true
-}
-
-resource "aws_nat_gateway" "gw" {
-  count = length(var.cidr_block_public_subnet)
-  allocation_id = aws_eip.nat_gw_eip_1.id
-  subnet_id     = aws_subnet.public_1.id
-}
-
-resource "aws_route_table" "nated_1" {
+resource "aws_route_table" "private" {
+    count = length(var.cidr_block_private_subnet)
     vpc_id = aws_vpc.this.id
     route {
         cidr_block = "0.0.0.0/0"
-        nat_gateway_id = aws_nat_gateway.gw_1.id
+        gateway_id = aws_internet_gateway.this.id
     }
     tags = {
-        Name = "${var.name}-nated-rt-1"
+        Name = "${var.name}-public-rt"
     }
 }
-resource "aws_route_table" "nated_2" {
-    vpc_id = aws_vpc.this.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        nat_gateway_id = aws_nat_gateway.gw_2.id
-    }
-    tags = {
-        Name = "${var.name}-nated-rt-2"
-    }
-}
-resource "aws_route_table" "nated_3" {
-    vpc_id = aws_vpc.open_search.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        nat_gateway_id = aws_nat_gateway.gw_3.id
-    }
-    tags = {
-        Name = "${var.name}-nated-rt-3"
-    }
-}
-resource "aws_route_table_association" "nated_1" {
-    subnet_id = aws_subnet.nated_1.id
-    route_table_id = aws_route_table.nated_1.id
-}
-resource "aws_route_table_association" "nated_2" {
-    subnet_id = aws_subnet.nated_2.id
-    route_table_id = aws_route_table.nated_2.id
-}
-resource "aws_route_table_association" "nated_3" {
-    subnet_id = aws_subnet.nated_3.id
-    route_table_id = aws_route_table.nated_3.id
+resource "aws_route_table_association" "private" {
+    count = length(var.cidr_block_private_subnet)
+    subnet_id      = "${aws_subnet.private[count.index].id}"
+    route_table_id = "${aws_route_table.private[count.index].id}"
 }
--- vars.tf
+++ vars.tf
@@ -8,7 +8,7 @@ variable "name" {
   description = "Name of the VPC"
 }
 
-variable "addtional_tags" {
+variable "additional_tags" {
     type = map
     default = {}
     description = "Additional tags to add"
@@ -22,4 +22,13 @@ variable "cidr_block_public_subnet" {
 variable "cidr_block_private_subnet" {
     type = list
     description = "A list of CIDR blocks for public subnets that fits inside the VPC CIDR. E.g. 10.1.3.0/24, 10.1.4.0/24, 10.1.5.0/24 if the VPC is 10.1.0.0/16"
+}
+
+variable "cidr_block_data_subnet" {
+    type = list
+    description = "A list of CIDR blocks for public subnets that fits inside the VPC CIDR. E.g. 10.1.6.0/24, 10.1.7.0/24, 10.1.8.0/24 if the VPC is 10.1.0.0/16"
+}
+variable "aws_region" { 
+    description = "The AWS Region to deploy into (e.g eu-west-1, eu-central-1"
+    type = string
 }
